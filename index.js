898649f60a7f636b3b328f47624e76d9e8f36db4
Winston Pemberton <winstonpemberton@gmail.com>
2021-04-19 22:15:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,18 +5,4 @@ function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 
-function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-}
 
-function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-}
-
-function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-}
-
-function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-}
