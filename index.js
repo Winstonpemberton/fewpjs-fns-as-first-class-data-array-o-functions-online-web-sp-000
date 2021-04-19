822e6042ce9f450964e2194c06a3750e50e5c7b8
Winston Pemberton <winstonpemberton@gmail.com>
2021-04-19 22:33:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function unleashDog(dogName, dogBreed) {
 let routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
 
 function exerciseDog(dogName, dogBreed) {
-  for(i = 0; i < routine.length; i++){
+  for(let i = 0; i < routine.length; i++){
     routine.map(func => func(dogName, dogBreed))
   }
 }
