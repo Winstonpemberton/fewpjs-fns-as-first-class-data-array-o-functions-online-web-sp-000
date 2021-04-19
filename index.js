4c445d5a4a4f67ae1e8faf47a022772ff0ab405c
Winston Pemberton <winstonpemberton@gmail.com>
2021-04-19 22:19:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function wakeDog(dogName, dogBreed) {
 function leashDog(dogName, dogBreed) {
   return `Leash ${dogName} the ${dogBreed}`;
 }
-"Walk to the park with Byron the poodle"
+
 function walkDog(dogName, dogBreed) {
   return `Walk to the park with ${dogName} the ${dogBreed}`;
 }
